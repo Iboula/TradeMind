85f404ec4fc7fc2068969292643160674f0ac041
Iboula <31216524+Iboula@users.noreply.github.com>
2026-07-17 21:42:43 -0400

    fix(knowledgehub): remove redundant vector namespace

--- src/Modules/KnowledgeHub/TradeMind.Modules.KnowledgeHub.Infrastructure/KnowledgeHubModule.cs
+++ src/Modules/KnowledgeHub/TradeMind.Modules.KnowledgeHub.Infrastructure/KnowledgeHubModule.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
-using Pgvector;
 using Pgvector.EntityFrameworkCore;
 using TradeMind.Modules.KnowledgeHub.Application;
 
